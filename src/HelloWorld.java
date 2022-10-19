ff58ddebef1dfa821178944866afea27617b194b
Singam Sandeep <115863975+sandysingam108@users.noreply.github.com>
2022-10-19 14:56:26 +0530

    initiate4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World, now, then, there");
+        System.out.println("Hello World, now, then, there. check");
     }
 }
